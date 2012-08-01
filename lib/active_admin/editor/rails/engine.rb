0417ce7ee3159d53c32fab040cc1ac0e2936f766
Rajarshi Nigam <raj@Rajarshis-MacBook-Pro.local>
2012-08-01 11:47:01 -0400

    Removing asset support

--- lib/active_admin/editor/rails/engine.rb
+++ lib/active_admin/editor/rails/engine.rb
@@ -4,7 +4,7 @@ module ActiveAdmin
       class Engine < ::Rails::Engine
         engine_name 'active_admin_editor'
         config.after_initialize do
-          require 'active_admin/editor/admin/assets'
+          # require 'active_admin/editor/admin/assets'
         end
       end
     end
